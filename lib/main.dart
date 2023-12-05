d91d5b2f5fa664e01def94d9d2b494b428355c57
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2023-12-05 11:13:35 +0100

    clase 1 formularios (details)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:agenda/data/agenda.json.dart';
 import 'package:agenda/models/agenda.data.dart';
+import 'package:agenda/pages/contactdetails.page.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda/pages/contacts.page.dart';
 import 'package:provider/provider.dart';
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
             appBarTheme: AppBarTheme(color: const Color.fromARGB(0, 0, 0, 0)),
             colorScheme: ColorScheme.fromSeed(
                 seedColor: Colors.teal, background: Colors.black)),
-        home: ContactsPage(),
+        //home: ContactsPage(),
+        home: ContactDetailsPage(),
       ),
     );
   }
--- lib/pages/contactdetails.page.dart
+++ lib/pages/contactdetails.page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, avoid_print, curly_braces_in_flow_control_structures
+
 import 'package:flutter/material.dart';
 
 class ContactDetailsPage extends StatefulWidget {
@@ -8,8 +10,80 @@ class ContactDetailsPage extends StatefulWidget {
 }
 
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  late TextEditingController emailContactController;
+
+  //keys para la reorganización manual de la lista (arrastrar elementos)
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailContactController = TextEditingController(text: "Valor inicial");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailContactController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalle de contacto"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.save))],
+      ),
+      body: Form(
+          child: Column(
+        children: [
+          TextFormField(
+            controller: emailContactController,
+            validator: _onValidateEmailContact,
+            decoration: InputDecoration(
+              labelText: "email",
+              hintText: "hint",
+              border: OutlineInputBorder(),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+
+  void _onSave() {
+    //se necesita un controlador para cada campo del formulario
+    if (_formKey.currentState!.validate()) {
+      print("PERFECTO, todos los campos estan bien");
+    } else {
+      print("Algun campo no está bien");
+    }
+    print(emailContactController.text);
+  }
+
+  void _saveChanges(BuildContext context) {
+    Navigator.of(context).pop<bool>(true);
+  }
+
+  void _cancelChanges(BuildContext context) {
+    Navigator.of(context).pop<bool>(false);
+  }
+
+  String? _onValidateObligatorio(String? value) {
+    //TODO implementar check email regex
+    if (value == null || value.trim().isEmpty) {
+      return "Campo obligatorio";
+    }
+    return null;
+  }
+
+  String? _onValidateEmailContact(String? value) {
+    //TODO implementar check email regex
+    if (value == null || value.trim().isEmpty) {
+      return "Campo obligatorio";
+    }
+    //buscar regex email
+
+    return null;
   }
 }

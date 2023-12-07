9a6efc8e19b041083ae7f2343a22d28a9c3d2e54
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2023-12-07 20:25:20 +0100

    marcar fav (no se actualiza automaticamente en el tab de favs)

--- lib/pages/contactdetails.page.dart
+++ lib/pages/contactdetails.page.dart
@@ -16,91 +16,116 @@ class ContactDetailsPage extends StatefulWidget {
 }
 
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  late ContactData copy;
+
+  @override
+  void initState() {
+    super.initState();
+    copy = widget.contact.copyWith();
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    ContactData c = widget.contact; //por comodidad
+    //ContactData copy = widget.contact.copyWith();
     DateFormat format = DateFormat("MMM dd, yyyy");
 
-    String strLabels = c.labels
+    String strLabels = widget.contact.labels
             ?.map((e) => e[0].toUpperCase() + e.substring(1))
             .reduce((value, element) => element += ", $value") ??
         "n/a";
 
-    return ListenableBuilder(
-        listenable: widget.contact,
-        builder: (context, child) {
-          return Scaffold(
-              appBar: AppBar(
-                actions: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-                  IconButton(
-                      onPressed: () {
-                        setState(() => c.isFavorite = !c.isFavorite);
-                      },
-                      icon: Icon(c.isFavorite ? Icons.star : Icons.star_border))
-                ],
-              ),
-              body: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: CircleAvatar(
-                      radius: 100,
-                      child: LabelIcon(labels: c.labels, size: 100),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Center(
-                      child: Text(
-                        "${c.name} ${c.surname}",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: theme.textTheme.headlineMedium!.fontSize),
+    return WillPopScope(
+      onWillPop: () {
+        //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("prueba")));
+        widget.contact.isFavorite = copy.isFavorite;
+        widget.contact.modification = DateTime.now();
+        Navigator.pop(context);
+        return Future.value(true);
+      },
+      child: ListenableBuilder(
+          listenable: widget.contact,
+          builder: (context, child) {
+            return Scaffold(
+                appBar: AppBar(
+                  actions: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            copy.isFavorite = !copy.isFavorite;
+                          });
+                        },
+                        icon: Icon(
+                            copy.isFavorite ? Icons.star : Icons.star_border))
+                  ],
+                ),
+                body: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: CircleAvatar(
+                        radius: 100,
+                        child:
+                            LabelIcon(labels: widget.contact.labels, size: 100),
                       ),
                     ),
-                  ),
-                  Divider(thickness: 2),
-                  ListTile(
-                      title: Text("Correo electrónico",
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Center(
+                        child: Text(
+                          "${widget.contact.name} ${widget.contact.surname}",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize:
+                                  theme.textTheme.headlineMedium!.fontSize),
+                        ),
+                      ),
+                    ),
+                    Divider(thickness: 2),
+                    ListTile(
+                        title: Text("Correo electrónico",
+                            style: theme.textTheme.titleMedium),
+                        subtitle: Text(widget.contact.email ?? "n/a",
+                            style: theme.textTheme.headlineSmall),
+                        trailing: Icon(Icons.email)),
+                    Divider(thickness: 2),
+                    ListTile(
+                        title: Text("Teléfono",
+                            style: theme.textTheme.titleMedium),
+                        subtitle: Text(widget.contact.phone ?? "n/a",
+                            style: theme.textTheme.headlineSmall),
+                        trailing: Icon(Icons.phone)),
+                    Divider(thickness: 2),
+                    ListTile(
+                      title: Text("Nacimiento",
                           style: theme.textTheme.titleMedium),
-                      subtitle: Text(c.email ?? "n/a",
+                      subtitle: Text(
+                          widget.contact.birthdate != null
+                              ? format.format(widget.contact.birthdate!)
+                              : "n/a",
                           style: theme.textTheme.headlineSmall),
-                      trailing: Icon(Icons.email)),
-                  Divider(thickness: 2),
-                  ListTile(
+                    ),
+                    /*Row(
+                      children: [ListTile(title: Text("a")), Text("b")],
+                    )*/
+                    Divider(thickness: 2),
+                    ListTile(
                       title:
-                          Text("Teléfono", style: theme.textTheme.titleMedium),
-                      subtitle: Text(widget.contact.phone ?? "n/a",
-                          style: theme.textTheme.headlineSmall),
-                      trailing: Icon(Icons.phone)),
-                  Divider(thickness: 2),
-                  ListTile(
-                    title:
-                        Text("Nacimiento", style: theme.textTheme.titleMedium),
-                    subtitle: Text(
-                        c.birthdate != null
-                            ? format.format(c.birthdate!)
-                            : "n/a",
-                        style: theme.textTheme.headlineSmall),
-                  ),
-                  /*Row(
-                    children: [ListTile(title: Text("a")), Text("b")],
-                  )*/
-                  Divider(thickness: 2),
-                  ListTile(
-                    title:
-                        Text("Etiquetas", style: theme.textTheme.titleMedium),
-                    subtitle:
-                        Text(strLabels, style: theme.textTheme.headlineSmall),
-                  ),
-                  Divider(thickness: 2),
-                  Center(child: Text("Añadido en ${c.creation}")),
-                  SizedBox(height: 5),
-                  Center(child: Text("Editado en ${c.creation}")),
-                ],
-              ));
-        });
+                          Text("Etiquetas", style: theme.textTheme.titleMedium),
+                      subtitle:
+                          Text(strLabels, style: theme.textTheme.headlineSmall),
+                    ),
+                    Divider(thickness: 2),
+                    Center(
+                        child: Text("Añadido en ${widget.contact.creation}")),
+                    SizedBox(height: 5),
+                    Center(
+                        child:
+                            Text("Editado en ${widget.contact.modification}")),
+                  ],
+                ));
+          }),
+    );
   }
 }
